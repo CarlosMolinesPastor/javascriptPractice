d065ad58dfd2dc5d4f7b1287f2f77eae03f665f3
CarlosMolinesPastor <carmolpas@alu.edu.gva.es>
2024-03-21 13:06:39 +0100

    cadenas.js added new appends

--- cadenas.js
+++ cadenas.js
@@ -17,3 +17,39 @@ regnglones para que sea leida facilmente\
 es importante hacerlo asi \
 ya sabes...";
 console.log(texto);
+
+// Metodos propios de las cadenas de texto
+// cadenaDeTexto.Metodo(Parametros)
+console.log("");
+console.log("#### METODOS PROPIOS DE CADENA DE TEXTO ####");
+console.log("");
+let cadena = "Mi nombre es Carlos";
+console.log(cadena);
+console.log("");
+console.log("Numero de caracteres, cadena.leght ", cadena.length);
+console.log("");
+console.log(
+  "Devuelve el caracter de la posicion 4 cadena.charAt(4)",
+  cadena.charAt(4),
+);
+console.log(
+  "Devuelve el caracter ASCII de la posicion cadena.charCodeAt(4)",
+  cadena.charCodeAt(4),
+);
+console.log(
+  "Accedemos al valor directamente como si fuera un array cadena[4]",
+  cadena[4],
+);
+console.log(
+  "Posicion en la cadena de 'nombre' indexOf()",
+  cadena.indexOf("nombre"),
+);
+console.log(
+  "Posicion en la cadena de 'por' indexOf(), no lo encuentra devuelve -1",
+  cadena.indexOf("por"),
+);
+console.log(
+  "Possicion de la cadena 'nombre' search() que hace lo mismo que indexOf(), pero admite segundo parametro \
+  que indica en que posicion empezar a buscar.",
+  cadena.search("nombre"),
+);
